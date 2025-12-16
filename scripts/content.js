bfc086f93dc8189c7471f355665de0765dce7898
Sabio <gamester.sad@gmail.com>
2025-12-16 18:20:24 +0000

    revamp code for first qol change to flavortown- for real!

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,13 @@
 function initialize() {
   const topCollabDiv = document.querySelector(".top-collab img");
-  if (!topCollabDiv) {
-    return;
+  if (topCollabDiv) {
+    const spicetownIcon = document.createElement("img");
+    spicetownIcon.src = chrome.runtime.getURL("/images/hc-gh&st-collab.png");
+    spicetownIcon.style.height = "45px";
+    spicetownIcon.style.width = "auto";
+
+    topCollabDiv.insertAdjacentElement("afterend", spicetownIcon);
   }
-
-  const spicetownIcon = document.createElement("img");
-  spicetownIcon.src = chrome.runtime.getURL("/images/hc-gh&st-collab.png");
-  spicetownIcon.style.height = "45px";
-  spicetownIcon.style.width = "auto";
-
-  if (!spicetownIcon) {
-    return;
-  }
-
-  topCollabDiv.insertAdjacentElement("afterend", spicetownIcon);
 }
 
 initialize();
